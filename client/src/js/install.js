ccd94c635a0a5c02d6d73dc812a25e0ca0033f90
Rey G. <reyanna.garibay@gmail.com>
2024-07-31 21:07:57 -0500

    fixed typos and imports

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,13 +3,13 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-  window.defferedPrompt = event;
+  window.deferredPrompt = event;
   butInstall.classList.toggle('hidden', false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-  const promptEvent = window.defferedPrompt;
+  const promptEvent = window.deferredPrompt;
 
   if(!promptEvent) {
     return;
@@ -17,7 +17,7 @@ butInstall.addEventListener('click', async () => {
 
   promptEvent.prompt();
 
-  window.defferedPrompt = null;
+  window.deferredPrompt = null;
 
   butInstall.classList.toggle('hidden', true);
 });
@@ -25,5 +25,5 @@ butInstall.addEventListener('click', async () => {
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
   console.log('PWA installed succesfully!')
-  window.defferedPrompt = null;
+  window.deferredPrompt = null;
 });
